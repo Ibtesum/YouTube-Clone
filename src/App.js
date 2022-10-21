890783728891e4f8b2924b5c72b65417e9f3fa08
Ibtesum Reza Aninda <aninda7152@gmail.com>
2022-10-21 19:28:14 +0600

    upto 56.30

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import Feed from "./components/Feed";
-import VideoDetail from "./components/VideoDetail";
-import ChannelDetail from "./components/ChannelDetail";
-import SearchFeed from "./components/SearchFeed";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 import SearchBar from "./components/SearchBar";
-
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: "#000" }}>
+    <Box sx={{ backgroundColor: "#333" }}>
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Feed />} />
